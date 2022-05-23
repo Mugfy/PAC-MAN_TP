ed34f6b87da63d589ab019b48a24797d7684efb6
Mugfy <104918033+Mugfy@users.noreply.github.com>
2022-05-23 09:07:04 +0200

    Update observercollision.cpp

--- observercollision.cpp
+++ observercollision.cpp
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
 #include "observercollision.h"
 
 ObserverCollision::ObserverCollision()
 {
 }
-
-=======
-#include "observercollision.h"
-
-ObserverCollision::ObserverCollision()
-{
-}
-
->>>>>>> 71185399f1a3578c7b1322122d90cb1d78cc9df8
